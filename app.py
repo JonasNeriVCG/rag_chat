a98f5747a4c28bfeee4808154660522c01634345
Jonas Litardi Neri <litardineri@Jonass-MacBook-Pro.local>
2024-12-09 11:59:22 +0100

    changed LLM used

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ import sys
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
-from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(model="gpt-4o", temperature=0)
+# Initialize ChatOpenAI with GPT-4
+llm = ChatOpenAI(model="gpt-4", temperature=0)
 
 main_prompt = ChatPromptTemplate.from_template(
     """
@@ -30,7 +31,6 @@ def ensure_faiss_database():
     faiss_files = [f for f in os.listdir() if f.endswith(".faiss")]
     if not faiss_files:
         print("No FAISS database found. Running preprocessing and database creation scripts...")
-        # Use the current Python executable explicitly
         python_executable = sys.executable
         subprocess.run([python_executable, "preprocess.py"], check=True)
         subprocess.run([python_executable, "db.py"], check=True)
@@ -78,7 +78,7 @@ def generate_stepback_question(question):
     the underlying principles needed to answer the specific question.
     If you don't recognize a word or acronym to not try to rewrite it.
     Write concise questions.: {question}"""
-    stepback_question = llm.invoke(prompt).strip()
+    stepback_question = llm.invoke(prompt).content.strip()
     return stepback_question
 
 def ask_question(db, question, top_k=5):
@@ -91,7 +91,6 @@ def ask_question(db, question, top_k=5):
         chunk_content = doc.page_content
         references = doc.metadata.get("references", "N/A")
         
-        # Constructing metadata info with line breaks
         metadata_info = (
             f"Title: {doc.metadata.get('title', 'N/A')}\n"
             f"Author: {doc.metadata.get('author', 'N/A')}\n"
@@ -105,7 +104,7 @@ def ask_question(db, question, top_k=5):
     context = "\n\n".join([doc.page_content for doc, _ in docs_and_scores])
     prompt_input = {"context": context, "input": question}
     response = llm.invoke(main_prompt.format_prompt(**prompt_input).to_string())
-    return response.strip(), "\n\n".join(formatted_chunks), stepback_question
+    return response.content.strip(), "\n\n".join(formatted_chunks), stepback_question
 
 def generate_response(db_name, question):
     db_path = f"{db_name}.faiss"
